b603a871ea54c68e57345009288b05966c585dda
Kasumi Iguchi <kasumiin.igc175@gmail.com>
2021-06-12 08:21:45 +0900

    change2

---
+++ wp-content/plugins/snow-monkey-forms/dist/js/blocks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => 'f220610cc82b9d6a59cc11bf7ea732a9');
---
+++ wp-content/plugins/snow-monkey-forms/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.5.3',
+    'version' => '1.5.3.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '42790f5bc4a975c2504bffea02a3dd1136fe70fa',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.5.3',
+      'version' => '1.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42790f5bc4a975c2504bffea02a3dd1136fe70fa',
+    ),
+  ),
+);
